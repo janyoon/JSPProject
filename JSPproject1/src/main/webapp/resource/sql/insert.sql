6c5333a7432a0fb456098c0a1ca4f4ff3c6238cf
it <it@DESKTOP-TUAOFH1>
2024-02-23 16:43:51 +0900

    2.23 sql

---
+++ JSPproject1/src/main/webapp/resource/sql/insert.sql
@@ -0,0 +1,22 @@
+INSERT ALL
+    INTO PRODUCT VALUES
+        ('P1234','Cesca chair',
+        1700000,
+        'Satin, Wood, Rattan',
+        'chair',
+        'knoll',
+        1000,
+        'D57 x W46 x H46/79cm',
+        'P1234,jpg')
+    INTO PRODUCT VALUES
+        ('P1235','Hauge table',
+        1949000,'oak wood, steel, veneer, lacquered',
+        'table','BoConcept',1000,
+        'D99 x W160 x H74.5cm','P1235.jpg')
+    INTO PRODUCT VALUES
+        ('P1236','Bolzano sofa',
+        4000000,'2.5seater, RightDirection, bucle(razio)fabric',
+        'sofa','BoConcept',1000,
+        'D91 x W176 x H42/76cm','P1236.jpg')        
+
+SELECT * FROM DUAL;  
